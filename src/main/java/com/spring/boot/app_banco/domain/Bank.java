1d0440818edb1c7120550273325a9a0be4d00bbb
Maximiliano-G17 <maxigimenez1792@gmail.com>
2020-12-21 11:37:06 -0300

    subiendo proyecto

---
+++ src/main/java/com/spring/boot/app_banco/domain/Bank.java
@@ -0,0 +1,5 @@
+package com.spring.boot.app_banco.domain;
+
+public class Bank {
+
+}
---
+++ src/main/java/com/spring/boot/app_banco/domain/Persona.java
@@ -0,0 +1,66 @@
+package com.spring.boot.app_banco.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "persona")
+public class Persona {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String surname;
+	private int dni;
+	private String address;
+	private double balance;
+		
+	public Persona() {
+		super();
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public int getDni() {
+		return dni;
+	}
+	public void setDni(int dni) {
+		this.dni = dni;
+	}
+	public String getaddress() {
+		return address;
+	}
+	public void setaddress(String address) {
+		this.address = address;
+	}
+	public double getbalance() {
+		return balance;
+	}
+	public void setbalance(double balance) {
+		this.balance = balance;
+	}
+	
+	@Override
+	public String toString() {
+		return "Persona [id=" + id + ", dni=" + dni + "]";
+	}	
+}
